cda088c097635b33bd6e8c0dd97e473cb5a94cab
kajela <titoyut56@gmail.com>
2023-03-24 11:33:40 -0400

    add the initial files

---
+++ src/main/java/dao/ClientDao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import models.Client;
+
+import java.awt.*;
+import java.util.List;
+
+public interface ClientDao {
+    //CREATE
+    void add(Client client);
+    //READ
+    List<Client> getAll();
+    Client findById(int Id);
+    //UPDATE
+    void update(int id, String name, String email, String tel, String car, int serviceId);
+    //DELETE
+    void deleteById(int id);
+    void clearAll();
+
+}

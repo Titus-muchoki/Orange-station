373e7b6d92d3bafb46b7cc45d7e6b17e804af918
kajela <titoyut56@gmail.com>
2023-03-25 10:26:53 -0400

    add the initial files

---
+++ src/main/java/dao/Sql2oServiceDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import models.Client;
+import models.Service;
+
+import java.util.List;
+
+public class Sql2oServiceDao implements ServiceDao {
+
+    @Override
+    public void add(Service service) {
+
+    }
+
+    @Override
+    public List<Service> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Client> getAllClientsByService() {
+        return null;
+    }
+
+    @Override
+    public Service findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}

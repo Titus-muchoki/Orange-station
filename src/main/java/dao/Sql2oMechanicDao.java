6cb7d7e766b7417f5314fed55173eeb7ff2c6693
kajela <titoyut56@gmail.com>
2023-03-25 15:13:16 -0400

    add the initial files

--- src/main/java/dao/Sql2oMechanicDao.java
+++ src/main/java/dao/Sql2oMechanicDao.java
@@ -1,11 +1,61 @@
 package dao;
 
+import models.Client;
+import models.Mechanic;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
-public class Sql2oMechanicDao {
+import java.util.List;
+
+public class Sql2oMechanicDao implements MechanicDao {
     private final Sql2o sql2o;
 
     public Sql2oMechanicDao(Sql2o sql2o) {
         this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Mechanic mechanic) {
+    String sql = "INSERT INTO mechanics(name, charges, clientid)VALUES(:name, :charges, :clientId)";
+    try (Connection con = sql2o.open()){
+        int id = (int) con.createQuery(sql, true)
+                .bind(mechanic)
+                .executeUpdate()
+                .getKey();
+        mechanic.getId();
+    }catch (Sql2oException ex){
+        System.out.println(ex);
+    }
+    }
+
+    @Override
+    public List<Mechanic> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Client> getAllMechanicsByClient(int mechanicId) {
+        return null;
+    }
+
+    @Override
+    public Mechanic findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String charges) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
     }
 }

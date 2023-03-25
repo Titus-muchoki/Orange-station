29f146b88e86863699c4c4d6f0d37f5c1e6c52a4
kajela <titoyut56@gmail.com>
2023-03-25 06:30:19 -0400

    add the initial files

--- src/main/java/dao/Sql2oClientDao.java
+++ src/main/java/dao/Sql2oClientDao.java
@@ -1,12 +1,32 @@
 package dao;
 
 import models.Client;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
 
 import java.util.List;
 
 public class Sql2oClientDao implements ClientDao{
+    private final Sql2o sql2o;
+
+    public Sql2oClientDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
     @Override
     public void add(Client client) {
+        String sql = "INSERT INTO clients(name, email, tel, car, serviceId)VALUES(:name, :email, :tel, :car, :serviceId)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(client)
+                    .executeUpdate()
+                    .getKey();
+            client.getId();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
 
     }
 

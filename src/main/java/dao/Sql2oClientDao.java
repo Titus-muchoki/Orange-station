e428427209fb8fd0d3fad03847ad87a3abf7972c
kajela <titoyut56@gmail.com>
2023-03-25 06:46:37 -0400

    add the initial files

--- src/main/java/dao/Sql2oClientDao.java
+++ src/main/java/dao/Sql2oClientDao.java
@@ -67,7 +67,14 @@ public class Sql2oClientDao implements ClientDao{
 
     @Override
     public void deleteById(int id) {
-
+    String sql = "DELETE from clients WHERE id = :id";
+    try(Connection con = sql2o.open()) {
+        con.createQuery(sql)
+                .addParameter("id", id)
+                .executeUpdate();
+    }catch (Sql2oException ex){
+        System.out.println(ex);
+    }
     }
 
     @Override

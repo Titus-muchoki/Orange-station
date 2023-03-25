dac3eb80257c7eafa63f35e0291ba433aaf34f9f
kajela <titoyut56@gmail.com>
2023-03-25 10:32:04 -0400

    add the initial files

--- src/main/java/dao/Sql2oServiceDao.java
+++ src/main/java/dao/Sql2oServiceDao.java
@@ -2,14 +2,31 @@ package dao;
 
 import models.Client;
 import models.Service;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class Sql2oServiceDao implements ServiceDao {
+        private final Sql2o sql2o;
+
+    public Sql2oServiceDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
 
     @Override
     public void add(Service service) {
-
+        String sql = "INSERT INTO services(name)VALUES(:name)";
+        try (Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(service)
+                    .executeUpdate()
+                    .getKey();
+            service.getId();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 
     @Override

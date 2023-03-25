bdec8031fe7f8f11c2b39502d337942cc243067f
kajela <titoyut56@gmail.com>
2023-03-25 11:13:42 -0400

    add the initial files

--- src/test/java/dao/Sql2oServiceDaoTest.java
+++ src/test/java/dao/Sql2oServiceDaoTest.java
@@ -1,5 +1,7 @@
 package dao;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -13,5 +15,15 @@ public class Sql2oServiceDaoTest {
         Sql2o sql2o = new Sql2o(connectionString, "kajela", "8444");
         serviceDao = new Sql2oServiceDao(sql2o);
         con = sql2o.open();
+    }
+    @After
+    public void tearDown() throws Exception{
+        serviceDao.clearAll();
+        System.out.println("clearing database");
+    }
+    @AfterClass
+    public void shutDown() throws Exception{
+        con.close();
+        System.out.println("closing database");
     }
 }
